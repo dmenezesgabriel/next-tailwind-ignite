5204be654591d5b28aec5e07fd0e00fce447cdde
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-12-29 20:43:43 -0300

    feat: add button component variants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,19 +23,12 @@ export default function Home() {
             </span>
           </div>
           <div className="flex items-center gap-2">
-            <button
-              type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-            >
+            <Button variant="outline" type="button">
               Cancel
-            </button>
-            <button
-              type="submit"
-              form="settings"
-              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-            >
+            </Button>
+            <Button variant="primary" type="submit" form="settings">
               Save
-            </button>
+            </Button>
           </div>
         </div>
         <form
@@ -157,39 +150,24 @@ export default function Home() {
                 </Select>
 
                 <div className="flex items-center gap-1">
-                  <button
-                    className="rounded-md p-2 hover:bg-zinc-50"
-                    type="button"
-                  >
+                  <Button variant="ghost" type="button">
                     <Bold className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                  </button>
-                  <button
-                    className="rounded-md p-2 hover:bg-zinc-50"
-                    type="button"
-                  >
+                  </Button>
+                  <Button variant="ghost" type="button">
                     <Italic className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                  </button>
-                  <button
-                    className="rounded-md p-2 hover:bg-zinc-50"
-                    type="button"
-                  >
+                  </Button>
+                  <Button variant="ghost" type="button">
                     <Link className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                  </button>
-                  <button
-                    className="rounded-md p-2 hover:bg-zinc-50"
-                    type="button"
-                  >
+                  </Button>
+                  <Button variant="ghost" type="button">
                     <List className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                  </button>
-                  <button
-                    className="rounded-md p-2 hover:bg-zinc-50"
-                    type="button"
-                  >
+                  </Button>
+                  <Button variant="ghost" type="button">
                     <ListOrdered
                       className="h-4 w-4 text-zinc-500"
                       strokeWidth={3}
                     />
-                  </button>
+                  </Button>
                 </div>
               </div>
 
@@ -215,16 +193,10 @@ export default function Home() {
           </div>
 
           <div className="flex items-center justify-end gap-2 pt-5">
-            <button
-              type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-            >
+            <Button variant="outline" type="button">
               Cancel
-            </button>
-            <Button
-              type="submit"
-              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-            >
+            </Button>
+            <Button variant="primary" type="submit">
               Save
             </Button>
           </div>
--- src/components/sidebar/profile.tsx
+++ src/components/sidebar/profile.tsx
@@ -1,8 +1,9 @@
 import { LogOut } from "lucide-react";
+import { Button } from "../button";
 
 export function Profile() {
   return (
-    <div className="grid-cols-profile grid items-center gap-3">
+    <div className="grid grid-cols-profile items-center gap-3">
       <img
         className="h-10 w-10 rounded-full"
         src="https://github.com/dmenezesgabriel.png"
@@ -16,9 +17,9 @@ export function Profile() {
           dmenezes.gabriel@gmail.com
         </span>
       </div>
-      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+      <Button type="button" variant="ghost">
         <LogOut className="h-5 w-5 text-zinc-500" />
-      </button>
+      </Button>
     </div>
   );
 }
