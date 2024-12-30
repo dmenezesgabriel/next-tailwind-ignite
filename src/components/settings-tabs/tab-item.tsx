c8e96ead1bb15d459e9b01dc075357ccf992908d
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-12-30 10:40:08 -0300

    feat: main content dark theme

--- src/components/settings-tabs/tab-item.tsx
+++ src/components/settings-tabs/tab-item.tsx
@@ -15,14 +15,14 @@ export function TabItem({ value, title, isSelected = false }: TabItemProps) {
       value={value}
       className="group relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 focus:outline-none data-[state=active]:text-violet-700"
     >
-      <span className="group-visible-focus:ring-2 group-visible-focus:ring-violet-400 group-visible-focus:ring-offset-4 whitespace-nowrap rounded">
+      <span className="group-visible-focus:ring-2 group-visible-focus:ring-violet-400 group-visible-focus:ring-offset-4 whitespace-nowrap rounded hover:text-violet-300 dark:text-zinc-400 dark:data-[state=active]:text-violet-300">
         {title}
       </span>
 
       {isSelected && (
         <motion.div
           layoutId="activeTab"
-          className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700"
+          className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700 dark:bg-violet-300"
         ></motion.div>
       )}
     </Tabs.Trigger>
